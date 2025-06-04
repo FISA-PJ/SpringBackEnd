10b6f7b79ea9740a4c901f01f49aae043d0f2c74
leojjw <leojjw@naver.com>
2025-06-04 14:37:33 +0900

    Jenkins 테스트

--- src/test/java/com/mysite/applyhome/ApplyHomeApplicationTests.java
+++ src/test/java/com/mysite/applyhome/ApplyHomeApplicationTests.java
@@ -11,7 +11,7 @@ class ApplyHomeApplicationTests {
 
     @Test
     void testJpa() {
-        for (int i = 1; i <= 300; i++) {
+        for (int i = 1; i <= 10; i++) {
             String subject = String.format("테스트 데이터입니다:[%03d]", i);
             String content = "내용무";
         }
